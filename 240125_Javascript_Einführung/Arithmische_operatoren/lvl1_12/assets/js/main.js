5e6551b4a2e8a1db5d15ec6425a387e79e499e4c
SuperCoder <supercoder@MacBook-Air-von-SuperCoder.local>
2024-01-25 18:31:34 +0100

    done exercise Dom

---
+++ 240125_Javascript_Einführung/Arithmische_operatoren/lvl1_12/assets/js/main.js
@@ -0,0 +1,6 @@
+console.log(5 * 10);
+
+let score = 5 + 5;
+
+console.log("Ergebnis" + score);
+console.log(10 * score);
---
+++ 240125_Javascript_Einführung/document.write/Dom lvl 1_1/assets/js/main.js
@@ -0,0 +1,12 @@
+document.write("<h1> Hello World </h1>");
+document.write("<p>Have a nice day </p>");
+
+let myText = "Hello again";
+document.write("<p>" + myText + "</p>");
+
+const firstName = " Zied ";
+const familyName = " Khemiri ";
+
+document.write(
+  "<h2>" + " Hi, ich bin Supercoder" + firstName + familyName + "</h2>"
+);
