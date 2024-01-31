b9af4df3a32154301150380bf6093c1cb792e539
Zied1981 <zkhemiri@gmail.com>
2024-01-31 21:29:46 +0100

    done last exercise

---
+++ 240131_Js_Conditionals/conditionals_lvl2_3/assets/js/main.js
@@ -0,0 +1,14 @@
+function math27(numb) {
+  if (numb > 27) {
+    let differenz = numb - 27;
+    let result = differenz * 2;
+
+    console.log(differenz);
+    console.log(result);
+  }
+}
+
+math27(100);
+math27(35);
+math27(74);
+math27(123);
---
+++ 240131_Js_Conditionals/conditionals_lvl2_4/assets/js/main.js
@@ -0,0 +1,11 @@
+function addMe(numb1, numb2) {
+  let total = numb1 + numb2;
+
+  if (numb1 === numb2) {
+    console.log(5);
+  } else {
+    console.log(total);
+  }
+}
+
+addMe(10, 10);
