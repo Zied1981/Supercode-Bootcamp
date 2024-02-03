827f4c0e1e44cadbdfc88cb705eb451a306c30f2
Zied1981 <zkhemiri@gmail.com>
2024-02-03 16:44:32 +0100

    change & update main.js on project

--- 240202_Strings_Methoden_Slice_PAP/Project_zeichenkettentrennung/assets/js/main.js
+++ 240202_Strings_Methoden_Slice_PAP/Project_zeichenkettentrennung/assets/js/main.js
@@ -6,19 +6,46 @@ function checking() {
   ).value;
   let frontOutput = document.body.querySelector(".frontoutput");
   const backOutput = document.body.querySelector(".backoutput");
-  /*   console.log(textInput);
-  console.log(cutInput);
-  console.log(cutEingabe);
-  console.log(frontOutput);
-  console.log(backOutput) */ if (
-    textInput.length >= 0 &&
-    cutInput.length >= 0
-  ) {
+  const warning = document.body.querySelector(".error");
+  //variante 1
+  //wenn die abfrage mit html required gelöst werden soll.
+  /*  if (cutEingabe === "davor") {
     let cuttedText = textInput.slice(0, textInput.indexOf(cutInput));
-    frontOutput.innerHTML = cuttedText;
     let backCuttedText = textInput.slice(textInput.indexOf(cutInput));
+
+    frontOutput.innerHTML = cuttedText;
     backOutput.innerHTML = backCuttedText;
   } else {
-    console.log("fisch");
+    let cuttedText = textInput.slice(0, textInput.indexOf(cutInput) + 1);
+    let backCuttedText = textInput.slice(textInput.indexOf(cutInput) + 1);
+
+    frontOutput.innerHTML = cuttedText;
+    backOutput.innerHTML = backCuttedText;
+} */
+
+  // wenn wir ein warning erscheinen soll wenn der user nichts eingibt oder etwas falsch eingibt!!!!
+  //variante 2
+  if (textInput.length > 0 && cutInput.length > 0) {
+    if (cutEingabe === "davor") {
+      let cuttedText = textInput.slice(0, textInput.indexOf(cutInput));
+      let backCuttedText = textInput.slice(textInput.indexOf(cutInput));
+
+      frontOutput.innerHTML = cuttedText;
+      backOutput.innerHTML = backCuttedText;
+    } else {
+      let cuttedText = textInput.slice(0, textInput.indexOf(cutInput) + 1);
+      let backCuttedText = textInput.slice(textInput.indexOf(cutInput) + 1);
+
+      frontOutput.innerHTML = cuttedText;
+      backOutput.innerHTML = backCuttedText;
+    }
+  } else {
+    warning.innerHTML = "Bitte geben Sie was ein !!!";
   }
 }
+
+/* if ((textInput.length = 0)) {
+  warning.innerHTML = "Bitte geben Sie eine zeichenkette an";
+} else if ((cutInput.length = 0)) {
+  warning.innerHTML = "Bitte geben Sie einen trenner an";
+} */
