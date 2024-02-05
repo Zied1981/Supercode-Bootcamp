721bb586eacd649416123fe079f1be54537833c3
Zied1981 <zkhemiri@gmail.com>
2024-02-05 12:04:46 +0100

    done exercise 1

---
+++ 240205_String_MethodP2_Arrow_Function/String_metho1_1/assets/js/main.js
@@ -0,0 +1,9 @@
+const firstName = "Zied";
+const lastName = "Khemiri";
+console.log(firstName.length);
+console.log(lastName.length);
+const fullName = firstName + lastName;
+/* const fullName = `${firstName}${lastName}`; */
+/* console.log(fullName); */
+
+console.log(fullName.length);
