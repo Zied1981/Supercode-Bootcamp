e052fc646e83e42b942846fe1d75c430737f2a64
Zied1981 <zkhemiri@gmail.com>
2024-02-06 13:10:39 +0100

    done exr1-2-3

---
+++ 240206_Mathmethods_MWST_Rechner/math_lvl1_2/assets/js/main.js
@@ -0,0 +1,13 @@
+const rechner = () => {
+  let array = 3.14;
+  let array1 = 193.4464;
+  let array2 = 0.8596433607;
+  let array3 = -2.940629089;
+
+  console.log(Math.round(array));
+  console.log(Math.round(array1));
+  console.log(Math.round(array2));
+  console.log(Math.round(array3));
+};
+
+rechner();
---
+++ 240206_Mathmethods_MWST_Rechner/math_lvl1_3/asset/js/main.js
@@ -0,0 +1,6 @@
+let randomNum = Math.random();
+console.log(randomNum);
+let randomNum110 = Math.ceil(Math.random() * 10);
+console.log(randomNum110);
+let randomNum1100 = Math.ceil(Math.random() * 100);
+console.log(randomNum1100);
