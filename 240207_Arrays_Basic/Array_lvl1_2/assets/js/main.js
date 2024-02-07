7c97f983840b4e8bd7de7c70e2bbdb97e1f92555
Zied1981 <zkhemiri@gmail.com>
2024-02-07 12:57:56 +0100

    done exer1-2-3

---
+++ 240207_Arrays_Basic/Array_lvl1_2/assets/js/main.js
@@ -0,0 +1,18 @@
+const person = ["zied", "khemiri", "nett"];
+const friends = ["marco", "imed", "fares "];
+const favoriteFood = ["pasta", "burger", "couscous"];
+
+person[0];
+person[1];
+person[2];
+console.log(person[0], person[1], person[2]);
+
+friends[0];
+friends[1];
+friends[2];
+console.log(friends[0], friends[1], friends[2]);
+
+favoriteFood[0];
+favoriteFood[1];
+favoriteFood[2];
+console.log(favoriteFood[0], favoriteFood[1], favoriteFood[2]);
---
+++ 240207_Arrays_Basic/Array_lvl1_3/assets/js/main.js
@@ -0,0 +1,6 @@
+const person = ["zied", "khemiri", "nett"];
+const friends = ["marco", "imed", "fares "];
+const favoriteFood = ["pasta", "burger", "couscous"];
+console.log(person.length);
+console.log(friends.length);
+console.log(favoriteFood.length);
