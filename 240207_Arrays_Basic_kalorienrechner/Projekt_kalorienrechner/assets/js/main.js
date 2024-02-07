83df5cd9375b2938a2686af0b06d463cbf48111c
Zied1981 <zkhemiri@gmail.com>
2024-02-07 15:26:35 +0100

    change css

---
+++ 240207_Arrays_Basic_kalorienrechner/Projekt_kalorienrechner/assets/js/main.js
@@ -0,0 +1,10 @@
+//to do
+//auslesen von inputs grösse/alter/gewicht/geschlecht
+// wert vom dropdownmenü;
+// berechung : mann oder frau
+// wenn mann dann 66.47 + (13.7 * Körpergewicht in kg)+
+//  (5 * Körpergröße in cm) – (6.8 * Alter in Jahren) = Grundumsatz
+// wenn frau dann berechnung:655.1 + (9.6 * Körpergewicht in kg) + (1.8 * Körpergröße in cm)
+// – (4.7 * Alter in Jahren) = Grundumsatz
+
+console.log(hallo);
