cc4b2a289f37417858eee677a69e4b369aae60f2
Zied1981 <zkhemiri@gmail.com>
2024-02-21 18:09:04 +0100

    change css

--- 240221_API_JSON/Gallerie_Project/assets/js/main.js
+++ 240221_API_JSON/Gallerie_Project/assets/js/main.js
@@ -1,4 +1,5 @@
 fetch("https://picsum.photos/v2/list")
+  /* fetch("https://picsum.photos/v2/list?page=4&limit=40") */
   .then((response) => response.json())
   .then((newArray) => {
     console.log(newArray);
@@ -22,6 +23,41 @@ fetch("https://picsum.photos/v2/list")
       document.querySelector(".result").appendChild(contentBox);
     });
   })
-  .catch((error) => {
-    "fehler", error;
-  });
+  .catch((error) => console.log("fehler"));
+
+/* 
+
+
+
+
+
+  let grid = document.querySelector(".picsum_grid");
+let refreshBtn = document.querySelector("#refresh");
+let number = 1;
+
+let fetchFunction = () => {
+  fetch(https://picsum.photos/v2/list?page=${number}&limit=100)
+    .then((response) => response.json())
+    .then((picsumData) => {
+      let result = picsumData
+        .map((singleImg) => {
+          return <div> 
+        <img src="${singleImg.download_url}" alt="Picsum Img">
+        <h2>${singleImg.author} </h2>
+        <a href="${singleImg.url}">See more</a>
+        </div>
+        ;
+        })
+        .join("");
+      grid.innerHTML = result;
+    })
+    .catch((error) => console.log("ein Fehler ist aufgetreten", error));
+};
+fetchFunction();
+
+refreshBtn.addEventListener("click", () => {
+  number++;
+  fetchFunction();
+});
+
+ */
