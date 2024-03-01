3fe755a9c0fdae600eb18b27bb9f3bae76a7997b
Zied1981 <zkhemiri@gmail.com>
2024-03-01 15:25:18 +0100

    updt

--- 240301_BananaBank_Project/src/components/Bankcounter/Bankcounter.jsx
+++ 240301_BananaBank_Project/src/components/Bankcounter/Bankcounter.jsx
@@ -3,27 +3,31 @@ import "./Bankcounter.css";
 const Bankcounter = () => {
   //imput einzahlbetrag;
   const [amount, SetAmount] = useState("");
+
   //kontostand
   const [balance, setBalance] = useState("");
+
   //func addieren
   const add = () => {
     setBalance(Number(amount + balance));
     console.log("yay");
     SetAmount("");
   };
-  // func substraieren
+  // func substraieren :wenn der amount beim abziehen kleiner oder gleich der balance.
   const sub = () => {
     amount <= balance
       ? setBalance(Number(balance - amount))
-      : window.alert("amount to be surrended ist bigger than your balance ");
+      : (src =
+          ""); /*  window.alert("amount to be surrended ist bigger than your balance "); */
+
     SetAmount("");
   };
 
   return (
     <section className="counter">
       <h1>Blue Banana Bank</h1>
+      <h3>Mein Girokonto</h3>
       <div>
-        <h3>Mein Girokonto</h3>
         <p className="output">{balance}</p>
 
         <input
@@ -45,3 +49,16 @@ const Bankcounter = () => {
 };
 
 export default Bankcounter;
+
+/* const node = document.createElement('p');
+  const textnode = document.createTextNode(
+    'Sie haben nicht genug Geld auf dem Konto'
+  );
+  const moneyAlert = node.appendChild(textnode);
+const geldAuszahlen = () => {
+    balance < amount
+      ? // ? window.alert('Sie haben nicht genug Geld auf dem Konto')
+        document.getElementById('1').appendChild(moneyAlert)
+      : setBalance(Number(balance - amount));
+    setAmount('');
+  }; */
