b4fd12f5131116187a954ab8770e57399f5573c5
Zied1981 <zkhemiri@gmail.com>
2024-03-01 11:46:05 +0100

    done

---
+++ 240301_BananaBank_Project/src/components/Bankcounter/Bankcounter.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./Bankcounter.css";
+const Bankcounter = () => {
+  //imput einzahlbetrag;
+  const [amount, SetAmount] = useState("");
+  //kontostand
+  const [balance, setBalance] = useState(0);
+  //func addieren
+  const add = () => {
+    setBalance(Number(amount + balance));
+    console.log("yay");
+    SetAmount("");
+  };
+  // func
+  const sub = () => {
+    amount <= balance
+      ? setBalance(Number(balance - amount))
+      : window.alert("amount to be surrended ist bigger than your balance ");
+    SetAmount("");
+  };
+
+  return (
+    <section className="counter">
+      <h1>Banana Bank</h1>
+      <div>
+        <h3>Mein Girokonto</h3>
+        <p className="output">{balance}</p>
+
+        <input
+          type="number"
+          placeholder=" Betrag in € "
+          id="number"
+          onChange={(event) => SetAmount(Number(event.target.value))}
+          value={amount}
+        />
+
+        <br />
+        <div className="btn">
+          <button onClick={add}>Einzahlen</button>
+          <button onClick={sub}>Auszahlen</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Bankcounter;
---
+++ 240301_BananaBank_Project/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Bankcounter from "../../components/Bankcounter/Bankcounter";
+import "./Home.css";
+const Home = () => {
+  return (
+    <section>
+      <Bankcounter />
+    </section>
+  );
+};
+
+export default Home;
