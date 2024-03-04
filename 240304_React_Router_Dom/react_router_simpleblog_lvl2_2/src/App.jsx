3c2c4a0da18af3965c8af53e58cdc3e3dd1523b2
Zied1981 <zkhemiri@gmail.com>
2024-03-04 20:49:25 +0100

    done 2

--- 240304_React_Router_Dom/react_router_simpleblog_lvl2_2/src/App.jsx
+++ 240304_React_Router_Dom/react_router_simpleblog_lvl2_2/src/App.jsx
@@ -6,6 +6,9 @@ import Blog from "./pages/Blog/Blog";
 import Mypets from "./pages/Mypets/Mypets";
 import Myplants from "./pages/Myplants/Myplants";
 import Myhobbies from "./pages/Myhobbies/Myhobbies";
+import Mywork from "./pages/Mywork/Mywork";
+import Mysocialmedia from "./pages/Socialmedia/Socialmedia";
+import Codinglife from "./pages/Codinglife/Codinglife";
 
 function App() {
   return (
@@ -17,10 +20,10 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/0" element={<Mypets />} />
           <Route path="/blog/1" element={<Myplants />} />
-          <Route path="/blog/2" element={<My />} />
+          <Route path="/blog/2" element={<Mywork />} />
           <Route path="/blog/3" element={<Myhobbies />} />
-          <Route path="/blog/4" element={<Myplants />} />
-          <Route path="/blog/5" element={<Myplants />} />
+          <Route path="/blog/4" element={<Mysocialmedia />} />
+          <Route path="/blog/5" element={<Codinglife />} />
         </Routes>
       </BrowserRouter>
     </section>
